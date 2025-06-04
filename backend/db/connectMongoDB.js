eca9c6c5bcc7aa9614842bfffd908c8b61c99581
Elias Anwari <eli-cpu>
2025-06-04 09:19:15 +0200

    1- auth controllers added

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
   }
 };
+
+export default connectMongoDB;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
-import connectMongoDB from "./config/db.js";
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to parse req.body as JSON
+app.use(express.urlencoded({ extended: true })); // to parse URL-encoded data
+
+app.use(cookieParser()); // to parse cookies
 
 app.use("/api/auth", authRoutes);
 
