d613ab384f9c63b7ca9056591cb92150037b5fc0
Elias Anwari <eli-cpu>
2025-06-06 20:57:38 +0200

    1- post routes and controllers added

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { protectRoute } from "../middlewares/protectRoute.js";
+import {
+  getAllPosts,
+  createPost,
+  likeUnlikePost,
+  commentOnPost,
+  deletePost,
+} from "../controllers/post.controllers.js";
+
+const router = express.Router();
+
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getAllPosts);
+router.get("likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/:id", protectRoute, deletePost);
+
+export default router;
