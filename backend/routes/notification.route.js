e5682e9736c8ecd344be60c345b4d72409f6bc79
Elias Anwari <eli-cpu>
2025-06-07 13:11:54 +0200

    1- notification routes and controllers added

---
+++ backend/routes/notification.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getNotifications);
+router.delete("/", protectRoute, deleteNotifications);
+
+export default router;
