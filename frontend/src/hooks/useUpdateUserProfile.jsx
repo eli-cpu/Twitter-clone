689487292070e1fd900f3da90f2b3d1fa090bd3b
eli-cpu <e.anwari885@gmail.com>
2025-08-23 14:04:33 +0200

    useUpdateUserProfile hook added to keep the codebase clean

--- frontend/src/hooks/useUpdateUserProfile.jsx
+++ frontend/src/hooks/useUpdateUserProfile.jsx
@@ -1,6 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
-
 const useUpdateUserProfile = () => {
   const queryClient = useQueryClient();
 
